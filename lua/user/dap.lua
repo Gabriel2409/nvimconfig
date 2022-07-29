3a0d9feb0bcae63a2fe958559a9e34b5527f8f7f
Gabriel2409 <gabriel.cournell@gmail.com>
2022-07-29 11:52:52 +0200

    formatting

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,21 +1,21 @@
 local status_ok, dap = pcall(require, "dap")
 if not status_ok then
-  return
+	return
 end
 
 local ext_status_ok, dap_vscode = pcall(require, "dap.ext.vscode")
 if not ext_status_ok then
-  return
+	return
 end
 
-function file_exists(name)
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
+local function file_exists(name)
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 -- points to debugpy in the venv or the default installed by mason in case of failure
@@ -23,25 +23,25 @@ local venv_file = os.getenv("PWD") .. "/venv/bin/python"
 local default_config_file = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
 local file_to_use
 if file_exists(venv_file) then
-  file_to_use = venv_file
+	file_to_use = venv_file
 else
-  file_to_use = default_config_file
+	file_to_use = default_config_file
 end
 
 dap.adapters.python = {
-  type = "executable",
-  command = file_to_use,
-  args = { "-m", "debugpy.adapter" },
+	type = "executable",
+	command = file_to_use,
+	args = { "-m", "debugpy.adapter" },
 }
 -- by default will use the .vscode/launch.json file for configurations
 dap_vscode.load_launchjs(".vscode/launch.json", { debugpy = { "python" } })
 
 local virt_status_ok, dap_virtual_text = pcall(require, "nvim-dap-virtual-text")
 if virt_status_ok then
-  dap_virtual_text.setup()
+	dap_virtual_text.setup()
 end
 
 local ui_status_ok, dap_ui = pcall(require, "dapui")
 if ui_status_ok then
-  dap_ui.setup()
+	dap_ui.setup()
 end
