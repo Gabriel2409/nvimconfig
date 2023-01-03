1f97b7e58eb9fcef19f4f4ad4858c78e95d14e57
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-01-03 09:22:29 +0100

    updated plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -27,9 +27,6 @@ vim.cmd([[
 
 -- Install your plugins here
 lazy.setup({
-	-- Have packer manage itself
-	"wbthomason/packer.nvim",
-
 	-- Plugins required by a lot of plugins
 	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
 	"nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
