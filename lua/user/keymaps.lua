35c8bf695e08c1f27c70059df6a3578830584733
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-10-26 14:42:53 +0200

    change to options

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,37 +19,47 @@ keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
 vim.g.mapleader = " "
 --vim.g.maplocalleader = "\\"
 
+keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+
 -- move lines up and down
-keymap("n", "<A-k>", ":m.-2<CR>", opts)
-keymap("n", "<A-j>", ":m.+1<CR>", opts)
+keymap("n", "<A-k>", "<cmd>m.-2<CR>", opts)
+keymap("n", "<A-j>", "<cmd>m.+1<CR>", opts)
 
 -- keep previous yank when pasting over a text in visual mode
 --keymap("v", "p", '"_dP', opts)
 
 -- ctrl s to save without formatting
-keymap("n", "<c-s>", ":noautocmd w<CR>", {})
-keymap("i", "<c-s>", "<esc>:noautocmd w<CR>a", {})
+keymap("n", "<c-s>", "<cmd>noautocmd w<CR>", {})
+keymap("i", "<c-s>", "<esc><cmd>noautocmd w<CR>a", {})
 
 -- ctrl z suspends vim. To see suspended jobs, run jobs. To resume vim, run fg
 
 -- resize panes with arrow keys
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Up>", "<cmd>resize -2<CR>", opts)
+keymap("n", "<C-Down>", "<cmd>resize +2<CR>", opts)
+keymap("n", "<C-Left>", "<cmd>vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", "<cmd>vertical resize +2<CR>", opts)
 
 -- toggle comment
-keymap("n", "<C-_>", ":lua require('Comment.api').toggle.linewise.current()<CR>", opts)
-keymap("v", "<C-_>", ":lua require('Comment.api').toggle.linewise('V')<CR>", opts)
+keymap("n", "<C-_>", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("v", "<C-_>", "<cmd>lua require('Comment.api').toggle.linewise('V')<CR>", opts)
 
 -- tabout
 vim.cmd([[inoremap <expr> <Tab> search('\%#[]>)}''"`]', 'n') ? '<Right>' : '<Tab>']])
 
--- Navigate buffers
--- keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
--- keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+-- Navigate
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "[b", ":bprevious<CR>", opts)
 keymap("n", "]b", ":bnext<CR>", opts)
+
+-- Add undo break-points: each time the character is added, it breaks the undo sequence
+-- keymap("i", ";", ";<c-g>u", opts)
+-- keymap("i", ".", ".<c-g>u", opts)
+-- keymap("i", ";", ";<c-g>u", opts)
 
 -- Telescope
 keymap(
