18cd3753641bc8008203ec52696aa6456d295b9c
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-10-27 22:54:55 +0200

    added extra plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,6 @@ local opt = vim.opt
 opt.clipboard = "" -- use default clipboard for standard yanks
 opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
 opt.colorcolumn = "80,81,82,83,84,85,86,87,88" -- editor ruler (88 is black limit)
-opt.formatoptions = "jcroqlnt" -- tcqj
+opt.formatoptions = "jcqlnt"
 opt.scrolloff = 8 -- Lines of context
 opt.spelllang = { "en" }
