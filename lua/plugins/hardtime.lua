89def58f67fd777a10a93ccf8be9abf57bf1de23
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-02 10:27:00 +0100

    Fix hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,3 +1,11 @@
+-- it bugs with lazyvim if we enable it directly so I only activate it after 1 sec
+vim.cmd([[
+  augroup HardTime
+    autocmd!
+    autocmd VimEnter * call timer_start(1000, { tid -> execute('lua require("hardtime").enable()')})
+  augroup END
+]])
+
 return {
   -- can't seem to enable it at startup for some reason
   {
