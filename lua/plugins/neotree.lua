03fdd0e4bd0d32b4c63c07b9e6565d67f9e33073
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-07 13:59:35 +0100

    Show dot files neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+      },
+    },
     window = {
       position = "left",
       mappings = {
