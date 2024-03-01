d05565cfd55bfda3ab0a7559ca5cafaf60906cfc
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-01 08:55:32 +0100

    Add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,26 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  opts = {
+    menu = {
+      width = vim.api.nvim_win_get_width(0) - 4,
+    },
+  },
+  keys = {
+    {
+      "<leader>H",
+      function()
+        require("harpoon"):list():append()
+      end,
+      desc = "Harpoon file",
+    },
+    {
+      "<leader>h",
+      function()
+        local harpoon = require("harpoon")
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      desc = "Harpoon quick menu",
+    },
+  },
+}
