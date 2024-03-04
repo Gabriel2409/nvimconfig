0bd809db07460d885bf0dae59f706fa27efab37f
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-04 11:37:01 +0100

    Move files to nvim-lazyvim folder

---
+++ nvim-lazyvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- https://github.com/neovim/neovim/issues/23291#issuecomment-1560742827
+-- require("vim.lsp._watchfiles")._watchfunc = function(_, _, _)
+--   return true
+-- end
