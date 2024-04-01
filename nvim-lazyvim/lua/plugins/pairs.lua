70fc7b513384f185e4cbd49ed831f2310b01a287
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-01 12:35:29 +0200

    Remove codeium for now

--- nvim-lazyvim/lua/plugins/pairs.lua
+++ nvim-lazyvim/lua/plugins/pairs.lua
@@ -2,15 +2,14 @@
 return {
   -- disabled for now as it seems to conflict with copilot
   'echasnovski/mini.pairs',
-  enabled = false,
-  -- opts = {
-  --   mappings = {
-  --     ["("] = { action = "open", pair = "()", neigh_pattern = ".[%W]" },
-  --     ["["] = { action = "open", pair = "[]", neigh_pattern = ".[%W]" },
-  --     ["{"] = { action = "open", pair = "{}", neigh_pattern = ".[%W]" },
-  --     ["`"] = { action = "closeopen", pair = "``", neigh_pattern = ".[%W]", register = { cr = false } },
-  --     ['"'] = { action = "closeopen", pair = '""', neigh_pattern = ".[%W]", register = { cr = false } },
-  --     ["'"] = { action = "closeopen", pair = "''", neigh_pattern = ".[%W]", register = { cr = false } },
-  --   },
-  -- },
+  opts = {
+    mappings = {
+      ['('] = { action = 'open', pair = '()', neigh_pattern = '.[%W]' },
+      ['['] = { action = 'open', pair = '[]', neigh_pattern = '.[%W]' },
+      ['{'] = { action = 'open', pair = '{}', neigh_pattern = '.[%W]' },
+      ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '.[%W]', register = { cr = false } },
+      ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '.[%W]', register = { cr = false } },
+      ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '.[%W]', register = { cr = false } },
+    },
+  },
 }
