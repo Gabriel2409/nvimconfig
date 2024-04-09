7bbf3313c7a7174e2ce5c00f9a5126cab3b6d425
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-04-09 13:27:42 +0200

    Update harpoon

--- nvim-lazyvim/lua/plugins/harpoon.lua
+++ nvim-lazyvim/lua/plugins/harpoon.lua
@@ -1,26 +1,12 @@
 return {
+  --  now available in LazyVim Extras
   'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  opts = {
-    menu = {
-      width = vim.api.nvim_win_get_width(0) - 4,
-    },
-  },
+  branch = "harpoon2",
   keys = {
-    {
-      '<leader>H',
-      function()
-        require('harpoon'):list():append()
-      end,
-      desc = 'Harpoon file',
-    },
-    {
-      '<leader>h',
-      function()
-        local harpoon = require 'harpoon'
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = 'Harpoon quick menu',
-    },
+    { "<leader>1", false },
+    { "<leader>2", false },
+    { "<leader>3", false },
+    { "<leader>4", false },
+    { "<leader>5", false },
   },
 }
