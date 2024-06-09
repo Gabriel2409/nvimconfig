7b36d706786d1ef50df3d1087d26e543f3c0f7ff
--show-origin <gabriel.cournelle@gmail.com>
2024-06-09 11:15:24 +0200

    update completion menu

--- nvim-lazyvim/lua/plugins/cmp.lua
+++ nvim-lazyvim/lua/plugins/cmp.lua
@@ -7,46 +7,41 @@ return {
   -- TAB TO AUTOCOMPLETE
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    'L3MON4D3/LuaSnip',
-    keys = function()
-      return {}
-    end,
-  },
   -- then: setup supertab in cmp
   {
     'hrsh7th/nvim-cmp',
     dependencies = {
       'hrsh7th/cmp-emoji',
+      'L3MON4D3/LuaSnip',
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-      end
-
       local luasnip = require 'luasnip'
       local cmp = require 'cmp'
 
       opts.mapping = vim.tbl_extend('force', opts.mapping, {
-        ['<CR>'] = {}, -- enter should not select
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.confirm { select = true }
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
+        -- enter should not select
+        ['<CR>'] = {},
+
+        -- tab to select current
+        -- ['<Tab>'] = cmp.mapping(function(fallback)
+        --   if cmp.visible() then
+        --     cmp.confirm { select = true }
+        --   elseif luasnip.expandable() then
+        --     luasnip.expand()
+        --   elseif luasnip.expand_or_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   elseif check_backspace() then
+        --     fallback()
+        --   else
+        --     fallback()
+        --   end
+        -- end, { 'i', 's' }),
+
         -- ['<C-k>'] = cmp.mapping.select_prev_item(),
         -- ['<C-j>'] = cmp.mapping.select_next_item(),
+
+        -- toggle completion menu
         ['<C-Space>'] = cmp.mapping {
           i = function()
             if cmp.visible() then
