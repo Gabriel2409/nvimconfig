3487d9f5ef342d18f042bcce51ec00b07a73e8c0
--show-origin <gabriel.cournelle@gmail.com>
2024-12-18 10:34:56 +0100

    Disable animations

--- nvim-lazyvim/lua/config/keymaps.lua
+++ nvim-lazyvim/lua/config/keymaps.lua
@@ -38,15 +38,6 @@ vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false })
 
 
 -- TODO: check if still relevant
--- open neotree directory for current file
-keymap(
-  'n',
-  '<leader>E',
-  '<cmd>Neotree reveal_force_cwd<cr>',
-  { desc = 'Open current file dir', noremap = true, silent = true }
-)
-
--- TODO: check if still relevant
 -- change directory (helps when searching with telescope within library)
 vim.api.nvim_set_keymap(
   'n',
