c34796b07ff28bee9d5babee3e304ca5dc31a5be
--show-origin <gabriel.cournelle@gmail.com>
2024-12-19 11:21:07 +0100

    Update fzf lua config

--- nvim-lazyvim/lua/plugins/fzf.lua
+++ nvim-lazyvim/lua/plugins/fzf.lua
@@ -6,6 +6,24 @@ return {
     -- end,
     keys = {
       { mode = { 'n' }, '<C-p>', "<cmd>lua require('fzf-lua').files()<CR>", desc = 'Overseer explorer' },
-    }
+    },
+    opts = {
+      files = {
+        actions = {
+          ["ctrl-r"] = { require "fzf-lua.actions".toggle_ignore },
+          ["ctrl-g"] = false,
+          ["alt-i"] = false,
+          ["alt-h"] = false,
+        }
+      },
+      grep  = {
+        actions = {
+          ["ctrl-r"] = { require "fzf-lua.actions".toggle_ignore },
+          ["alt-i"] = false,
+          ["alt-h"] = false,
+        }
+      }
+
+    },
   },
 }
