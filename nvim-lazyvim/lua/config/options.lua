57a89cb70ff370e161261f989d1d65ad999cfd0c
--show-origin <gabriel.cournelle@gmail.com>
2025-03-04 16:22:38 +0100

    Update to config

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -4,14 +4,13 @@
 
 local opt = vim.opt
 
-opt.clipboard = ''                             -- use default clipboard for standard yanks
-opt.cmdheight = 2                              -- more space in the neovim command line for displaying messages
+opt.clipboard = '' -- use default clipboard for standard yanks
+opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
 opt.colorcolumn = '80,81,82,83,84,85,86,87,88' -- editor ruler (88 is black limit)
 opt.formatoptions = 'jcqlnt'
-opt.scrolloff = 8                              -- Lines of context
-opt.spelllang = { 'en', 'fr' }
+opt.scrolloff = 8 -- Lines of context
+opt.spelllang = { 'en' }
 opt.swapfile = false
-
 
 -- disable animations
 vim.g.snacks_animate = false
